c60d85d4c7ffc675c565ca220f460f06c35c5ed0
PirasSalvatore <torep.92@gmail.com>
2025-03-20 16:04:25 +0100

    ADD header component

---
+++ react-dc-comics/src/components/Header.jsx
@@ -0,0 +1,28 @@
+export default function Header() {
+
+    //logic header
+
+
+    //markup
+    return (
+        <header>
+            <div className="container">
+                <img src="/img/dc-logo.png" alt="" />
+
+                <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="" className="active">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+            </div>
+
+        </header>
+    )
+}
