30672c71064988d60e675a226a0bf1a5cceb6f7b
PirasSalvatore <torep.92@gmail.com>
2025-03-20 17:40:32 +0100

    add main & add menu component

---
+++ react-dc-comics/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import Menu from './Menu'
+
+export default function Main() {
+    //logic
+
+    //markup
+    return (
+        <main>
+            <div className="container">
+                <div className="album"> --&gt; content goes here&lt;-- </div>
+            </div>
+
+            <Menu />
+        </main>
+    )
+}
---
+++ react-dc-comics/src/components/Menu.jsx
@@ -0,0 +1,33 @@
+export default function Menu() {
+    //logic
+
+    //markup
+    return (
+        <div className="menu">
+            <div className="container">
+                <ul>
+                    <li>
+                        <img src="/img/buy-comics-digital-comics.png" alt="" />
+                        <a href="">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <img src="/img/buy-comics-merchandise.png" alt="" />
+                        <a href="">DS MERCHANDISE</a>
+                    </li>
+                    <li>
+                        <img src="/img/buy-comics-subscriptions.png" alt="" />
+                        <a href="">SUBSCRIPTION</a>
+                    </li>
+                    <li>
+                        <img id="shopLocator" src="/img/buy-comics-shop-locator.png" alt="" />
+                        <a href="">COMIC SHOP LOCATOR</a>
+                    </li>
+                    <li>
+                        <img src="/img/buy-dc-power-visa.svg" alt="" />
+                        <a href="">DC POWER VISA</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
